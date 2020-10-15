387fb5a6ea7468ceb9fcfe5c0a57b9cf4067e1f1
sunhao <sunhao@indemind.cn>
2020-10-15 16:59:22 +0800

    update sign

--- matrix_mul_int.glsl
+++ matrix_mul_int.glsl
@@ -12,6 +12,12 @@ const float maxVal = 255.0;
 
 #define INPUT(x) ((x) * maxVal)
 #define OUTPUT(x) ((x) / maxVal)
+#define NEGATIVE(x) (256-(x))
+#define NEGATIVEf(x) (256.0-(x))
+#define KEEP_SIGNAL(x) (sign(x) >= 0 ? (x) : NEGATIVE(abs(x)))
+#define KEEP_SIGNALf(x) (sign(x) >= 0.0 ? (x) : NEGATIVEf(abs(x)))
+#define KEEP_SIGNALf_texture(x) KEEP_SIGNALf((x)*maxVal)
+
 
 vec4 Dequantize(const float zeroPoint, const float scale, vec4 a)
 {
@@ -27,6 +33,7 @@ vec4 Quantize(const float zeroPoint, const float scale, vec4 a)
 void main() {
     vec4 a = texture2D(A, tex_coord);
     vec4 b = texture2D(B, tex_coord);
+    
 
     b = Dequantize(zeroPointB, scaleB, b);
     a = Dequantize(zeroPointA, scaleA, a);
